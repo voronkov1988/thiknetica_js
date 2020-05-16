59263d77383906a3c074798b4e0ba572938c2869
voronkov88 <rybsoveti.ru@yandex.ru>
2020-05-17 01:38:30 +0300

    dop work

---
+++ 2lesson/second/4main.js
@@ -0,0 +1,35 @@
+const firstKeyCode = englishAlphabet.charCodeAt(0);
+const keyString = shuffle(englishAlphabet).join('');
+
+const encode = (resource) => {
+  resource = resource.toLocaleLowerCase();
+  let memo = '';
+  for (let index = 0; index < resource.length; index++) {
+      let code = resource.charCodeAt(index) - firstKeyCode;
+      if (code < 0 || code >= 26)
+          memo += ' ';
+      else
+          memo += keyString[code];
+  }
+  return memo;
+}
+
+const decode = (resource) => {
+  let memo = '';
+  for (let index = 0; index < resource.length; index++) {
+      let keyIndex = keyString.indexOf(resource[index]);
+      if (keyIndex === -1)
+          memo += ' ';
+      else
+          memo += String.fromCharCode(keyIndex + firstKeyCode);
+  }
+  return memo;
+}
+
+const resource = 'JavaScript engine';
+const encoded = encode(resource);
+const decoded = decode(encoded);
+
+console.log(`Resource: ${resource}`);
+console.log(`Encoded:  ${encoded}`);
+console.log(`Decoded:  ${decoded}`);
