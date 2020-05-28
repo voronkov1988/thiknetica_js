518ff70e41b506447f502ac12015055e79ae4fe9
voronkov88 <rybsoveti.ru@yandex.ru>
2020-05-29 00:17:26 +0300

    5 lesson second calculator update

--- 5lesson/доп/main.js
+++ 5lesson/доп/main.js
@@ -7,31 +7,47 @@ const resSpan = document.querySelector('.result span');
 
 
 td.forEach(item => {
-	item.addEventListener('click', ()=>{
-		//получаю полную строку выражения
-		calculatedString += item.textContent;
-		resSpan.textContent = calculatedString;
-		if(item.textContent === '=') {
-			resCalc.textContent = getResult(calculatedString);
-		}
-		if(item.textContent === 'C'){
-			calculatedString = '';
-			resCalc.textContent = 0;
-		}
-	});
+    item.addEventListener('click', () => {
+        //получаю полную строку выражения
+        calculatedString += item.textContent;
+        resSpan.textContent = calculatedString;
+        if (item.textContent === '=') {
+            resCalc.textContent = getResult(calculatedString);
+            calculatedString = +getResult(calculatedString);
+        }
+        if (item.textContent === 'C') {
+            calculatedString = '';
+            resCalc.textContent = 0;
+        }
+    });
 })
-function getResult(calculatedString){
-	//в конце строки вырезаю =
-	let resultString = calculatedString.substring(0, calculatedString.length - 1)
-	//определяю какой знак у выражения
-	let znak;
-	if(resultString.includes('+')) znak = `\+`;
-	if(resultString.includes('-')) znak = `\-`;
-	if(resultString.includes('/')) znak = '\/';
-	if(resultString.includes('*')) znak = `\*`;
-	//создаю массив исходя какой знак
-	let calcString = resultString.split(znak);
 
-	let res = new Function('return ' + `${calcString[0]} ${znak} ${calcString[1]}`)();
-	return res
+function getResult(calculatedString) {
+    let res = 0;
+    //в конце строки вырезаю =
+    let resultString = calculatedString.substring(0, calculatedString.length - 1)
+    //определяю какой знак у выражения
+    let znak;
+    if (resultString.includes('+')) znak = `+`;
+    if (resultString.includes('-')) znak = `-`;
+    if (resultString.includes('/')) znak = '/';
+    if (resultString.includes('*')) znak = `*`;
+    //создаю массив исходя какой знак
+    let calcString = resultString.split(znak);
+    console.log(calcString);
+    switch (znak) {
+        case '+':
+            res = +calcString[0] + +calcString[1];
+            break;
+        case '-':
+            res = +calcString[0] - +calcString[1];
+            break;
+        case '/':
+        	res = +calcString[0] / +calcString[1];
+        	break;
+        case '*':
+        	res = +calcString[0] * +calcString[1];
+    }
+    
+    return res
 }
