cac7efdc694bc184e8cc18cc52dced8263adade9
voronkov88 <rybsoveti.ru@yandex.ru>
2020-06-22 22:47:07 +0300

    bred 8 lessson

--- 8lesson/main.js
+++ 8lesson/main.js
@@ -3,111 +3,102 @@ class HtmlElement {
         this._target;
         this._template;
         this._styles;
+        this.variables;
         this._textContent;
-        this._variables;
     }
-    set target(dom){
-    	if(!dom)
-    		throw new Error('нужно ввести дом элемент');
-    	this._target = document.querySelector(dom);
-    	
-    }
-    get target(){
-    	return this._target;
-    }
-    set styles(data){
-    	this._styles = data;
-    	this.updateStyle(data);
-    }
-    set template(el){
-    	this._template = document.createElement(el);
-    	console.log(this._template);
-    	
-    }
-    set variables(data) {
-        this._variables = data;
-        this.newTemplate(data);
-    }
+    set target(dom) {
+        if (!dom)
+            throw new Error('нужно ввести дом элемент');
+        this._target = document.querySelector(dom);
 
-    _render(){
-    	this._target.append(this._template);
     }
-    render(){
-    	this._render();
-    }
-    _unrender(){
-    	this._template.remove();
-    }
-    unrender(){
-    	this._unrender();
-    }
-    updateStyle(stylesheet){
-    	this._template.style = stylesheet;
-    }
-    updateText(data){
-    	this.textContent = data;
-    	if(this.textContent){
-    		this._template.textContent += this.textContent;
-    	}
-    }
-    newTemplate = function (object) {
-        let updateTemplate;
-        const template = this._template;
+    get target() {
 
-        Object.keys(object).map(function(objectKey, index) {
-            const name = objectKey;
-            const value = object[objectKey];
-            console.log(value, name)
-            updateTemplate = `{{${name}}}`.replace(name, value)
-        });
-
-        this._variables = updateTemplate;
+        return this._target;
+    }
+    set styles(data) {
+        this._styles = data;
+        this.updateStyle(data);
+    }
+    set template(el) {
+        this._template = document.createElement(el);
+    }
+    _render() {
+        this._target.append(this._template);
+    }
+    render() {
+        this._render();
+    }
+    _unrender() {
+        this._template.remove();
+    }
+    unrender() {
+        this._unrender();
+    }
+    updateStyle(stylesheet) {
+        this._template.style = stylesheet;
+    }
+    updateText(data) {
+        this.textContent = data;
+        let variable = this.variables;
+        variable.prototype = Array;
+        //получаю строку названия свойства как в обьекте variables
+        let template = data.split('').slice(2,-2).join(''); // == qwerty
+        console.log(template);
+        //можно ли подставить значение template которое = 'qwerty'
+        this._template.textContent = variable.qwerty;
     }
 }
 
-class Div extends HtmlElement{
-	constructor(){
-		super();
-	}
-	set onClick(f){
-		this._template.addEventListener('click', f)
-	}
+class Div extends HtmlElement {
+    constructor() {
+        super();
+    }
+    set onClick(f) {
+        this._template.addEventListener('click', f)
+    }
 }
-class Input extends HtmlElement{
-	constructor (){
-		super();
-	}
-	set onInput(f){
-		this._template.addEventListener('input', f)
-	}
-	set onFocus (f){
-		this._template.addEventListener('focus', f)
-	}
+class Input extends HtmlElement {
+    constructor() {
+        super();
+    }
+    set onInput(f) {
+        this._template.addEventListener('input', f)
+    }
+    set onFocus(f) {
+        this._template.addEventListener('focus', f)
+    }
 }
 
 const one = new Div();
 
 one.target = 'h1';
 one.template = 'div';
-one.styles = 'color: red; font-size:2em';
-one.updateText('qwerty');
-one.updateText(' 123');
 
+one.styles = 'color: red; font-size:2em';
 one.variables = {
-    value: 'test',
-    value2: 'test2'
-};
+    output: 'Some text',
+    qwerty: 'some'
+}
+
 
 one.render();
+one.updateText('{{qwerty}}');
 one.onClick = () => console.log('onclick');
+console.log(one);
 // one.unrender();
 
 const two = new Input();
 two.target = 'h1';
 two.template = 'input';
-two.styles = 'color: red; font-size:2em';
+
+two.variables = {
+    output2: 'Some text2'
+}
 two.updateText('qwerty');
-two.updateText(' 123');
+two.styles = 'color: red; font-size:2em';
+
+
 two.render();
 two.onInput = () => console.log('onInput');
 two.onFocus = () => console.log('onFocus');
@@ -123,23 +114,23 @@ console.log(two);
 //     }
 
 //     set styles(stylesheet){
-//     	console.log(stylesheet);
-//     	this.stili = stylesheet;
-//     	console.log(this.stili);
-//     	this.template.style = this.stili
+//      console.log(stylesheet);
+//      this.stili = stylesheet;
+//      console.log(this.stili);
+//      this.template.style = this.stili
 //     }
 //     get styles(){
-//     	return this.stili;
+//      return this.stili;
 //     }
 
 //     createElement(el) {
-//     	console.log(this._target);
+//      console.log(this._target);
 //         let element = document.createElement(el);
 //         this.template = element;
 //         this._target.append(element)
 //     }
 //     createTextInElement(text){
-//     	this.template.textContent = text;
+//      this.template.textContent = text;
 //     }
 // }
 
