4bb14f5404d7cd733be76addd7c43ff50716cc47
voronkov88 <rybsoveti.ru@yandex.ru>
2020-07-02 16:36:34 +0300

    lesson10 ready

--- 10lesson/main.js
+++ 10lesson/main.js
@@ -1,35 +1,41 @@
 const API_KEY = 'FxnDLKI3So5xqmdmyP8JXGS51CtsJVPd';
 
-const getGif = (value) => {
-    return fetch(`http://api.giphy.com/v1/gifs/search?q=${value}&api_key=${API_KEY}`,{
-        cache: "force-cache"
-    }).then(result => {
-        if(!result.ok) {
-            throw new Error('Ошибка')
-        }
-        return result.json();
-    }).then(result =>{
-        if(result.meta.status !== 200) {
-            throw new Error(result.status)
-        }
-        let cache = {[value] : result.data};
-        return cache;
-    }).then(cache =>{
-        let div = document.querySelector('.img');
-        Object.keys(cache).map((key,value)=>{
-            const h2 = document.createElement('h2');
-            h2.textContent = Object.keys(cache);
-            div.append(h2);
-            cache[key].forEach((item,value)=>{
-                let element = document.createElement('div');
-                element.textContent = item.url;
-                div.append(element);
-            });
-            const hr = document.createElement('hr');
-            div.append(hr)
-        });
-    })
+const getGif = (text) => {
+    let cache = {};
+    if(cache[text]){
+        createElement(cache[text], text);
+    }else{
+        return fetch(`http://api.giphy.com/v1/gifs/search?q=${text}&api_key=${API_KEY}&limit=3`)
+            .then(result => {
+                if(!result.ok) {
+                    throw new Error('Ошибка')
+                }
+                return result.json();
+            }).then(result =>{
+                if(result.meta.status !== 200) {
+                    throw new Error(result.status)
+                }
+                cache[text] = result.data;
+                console.log(result.data);
+                console.log(cache[text]);
+                createElement(result.data, text);
+            })
+    }
 };
+
+function createElement(obj,textInput) {
+    let div = document.querySelector('.img');
+    const h2 = document.createElement('h2');
+    h2.textContent = textInput;
+    div.append(h2);
+    obj.forEach((item,value)=>{
+        let element = document.createElement('div');
+        element.textContent = item.url;
+        div.append(element);
+    });
+    const hr = document.createElement('hr');
+    div.append(hr)
+}
 
 function debounce(f, ms) {
     let isCooldown = false;
@@ -43,9 +49,10 @@ function debounce(f, ms) {
 function Input (input){
     let inputs = document.querySelector(input);
     inputs.addEventListener(input, ()=>{
-        setTimeout( () => f(inputs.value), 1000);
+        setTimeout( () => makeDelay(inputs.value), 1000);
     });
 }
 Input('input');
 
-let f = debounce(getGif, 1000);
+let makeDelay = debounce(getGif, 1000);
+
